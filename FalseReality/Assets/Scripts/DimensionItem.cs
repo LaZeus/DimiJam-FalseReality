7c92653b8755dd9f51f1dde773a79bbf3624b349
LaZeus <33258434+LaZeus@users.noreply.github.com>
2019-06-02 10:46:09 +0300

    props change color depending on their dimension

---
+++ FalseReality/Assets/Scripts/DimensionItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionItem : MonoBehaviour
+{
+    public enum Dimension
+    {
+        Normal,
+        Red,
+        Green,
+        Blue
+    }
+
+    public Dimension myDimension;
+}
--- FalseReality/Assets/Scripts/FarmSpot.cs
+++ FalseReality/Assets/Scripts/FarmSpot.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FarmSpot : MonoBehaviour
+public class FarmSpot : DimensionItem
 {
+    public enum FarmState
+    {
+        locked,
+        empty,
+        growing,
+        grown
+    }
+
+    [Space]
+
+    [SerializeField]
+    private FarmState myState;
+
+    [Space]
+
+    [SerializeField]
+    private SpriteRenderer myArt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeState();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void InitializeState()
+    {
+        switch (myDimension)
+        {
+            case Dimension.Normal:
+                myArt.color = Color.gray;
+                break;
+            case Dimension.Red:
+                myArt.color = Color.red;
+                break;
+            case Dimension.Green:
+                myArt.color = Color.green;
+                break;
+            case Dimension.Blue:
+                myArt.color = Color.blue;
+                break;
+            default:
+                break;
+        }
     }
 }
