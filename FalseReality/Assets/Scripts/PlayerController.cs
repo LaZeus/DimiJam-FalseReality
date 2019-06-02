2f0832ed6c608a36f8d53a942847b18f2583efd7
LaZeus <33258434+LaZeus@users.noreply.github.com>
2019-06-02 10:29:03 +0300

    player controller added

---
+++ FalseReality/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per fixed loop
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 inputs = new Vector2(
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical"));
+
+        inputs.Normalize();
+
+        rb.velocity = inputs * speed;
+    }
+}
